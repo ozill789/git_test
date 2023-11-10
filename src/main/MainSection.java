2f8878afe7f2822d64d6736a2d8a74e05c0cda34
lucas <dipa18930@gmial.com>
2023-11-10 17:30:57 +0630

    add new line

--- src/main/MainSection.java
+++ src/main/MainSection.java
@@ -5,6 +5,7 @@ public class MainSection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Inside main method...");
+ System.out.println("add new line");
 	}
 
 }
